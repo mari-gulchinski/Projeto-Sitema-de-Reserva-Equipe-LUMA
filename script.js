b94cc9bffc43bc9d4a1c096660dfff3b019c9762
mari-gulchinski <marianagulchinski0810@gmail.com>
2025-09-29 11:37:36 -0300

    subindo

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+SCRIPT DO SPRINT 01
+*/ 
+
+// abre o modal
+function abrirModal(){
+    const modal = document.getElementById("modalLogin")
+    if(modal && typeof modal.showModal === "function"){
+        modal.showModal();
+    }else{
+        alert("Modal não suportado neste navegador");
+    }
+
+}
